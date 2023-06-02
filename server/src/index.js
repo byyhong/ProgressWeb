54963b2eaa7f1f14493e183d5e1b2239677e6a33
byyhong <wzry24680@gmail.com>
2023-06-01 19:32:24 -0500

    Implemented Authorization System

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
+const cors = require('cors');
+
+const router = require('./router');
+//have ./ to tell the program that it is a file and not a package 
 
 dotenv.config();
 const app = express();
 
-app.get('/progress', (req, res) => {
-    res.send('Hello!');
-});
+app.use(express.json())
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.use(router);
+
+
 app.listen(8080);
